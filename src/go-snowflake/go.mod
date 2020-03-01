20c8c5cdcd19a09f547617d61f8c2b97c42befd2
Icecream <981464828@qq.com>
2020-03-01 22:35:03 +0800

    使用系统服务(service)安装、启动、停止go-snowflake

--- src/go-snowflake/go.mod
+++ src/go-snowflake/go.mod
@@ -2,6 +2,7 @@ module go-snowflake
 
 go 1.13
 
-replace icecream => ../icecream
-
-require github.com/IcecreamLee/goutils v1.0.0
+require (
+	github.com/IcecreamLee/goutils v1.0.2
+	github.com/kardianos/service v1.0.0
+)
--- src/go-snowflake/main.go
+++ src/go-snowflake/main.go
@@ -2,26 +2,80 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/IcecreamLee/goutils"
+	"github.com/kardianos/service"
+	"log"
 	"os"
 )
 
-var id *goutils.IDGenServ
-
 func main() {
-	loadConfig()
-	id.Run()
+	svcFlag := flag.String("s", "", "Control the system service.")
+	flag.Parse()
+
+	// 服务定义
+	svcConfig := &service.Config{
+		Name:        "GoSnowflake",
+		DisplayName: "Go Snowflake Service",
+		Description: "This is an Go service that generate id.",
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 接受命令行参数标志控制服务
+	if len(*svcFlag) != 0 {
+		err := service.Control(s, *svcFlag)
+		if err != nil {
+			log.Printf("Valid actions: %q\n", service.ControlAction)
+			log.Fatal(err)
+		}
+		return
+	}
+
+	err = s.Run()
+	if err != nil {
+		goutils.LogInfo(goutils.GetCurrentPath()+"run.log", "GoSnowflake run failure: "+err.Error())
+	}
 }
 
+// ID生成器http服务
+var idServ *goutils.IDGenServ
+
 // 加载配置文件
-func loadConfig() {
-	id = goutils.IDServSingleton()
+func init() {
+	idServ = goutils.IDServSingleton()
 	file, _ := os.Open(goutils.GetCurrentPath() + "conf.json")
 	defer file.Close()
 	decoder := json.NewDecoder(file)
-	err := decoder.Decode(id)
+	err := decoder.Decode(idServ)
 	if err != nil {
 		fmt.Println("Load Config Error:", err)
 	}
+}
+
+// Program structures.
+//  Define Start and Stop methods.
+type program struct{}
+
+// 服务运行
+func (p *program) Start(s service.Service) error {
+	goutils.LogInfo(goutils.GetCurrentPath()+"run.log", "GoSnowflake Starting...")
+	// Start should not block. Do the actual work async.
+	idServ = goutils.IDServSingleton()
+	go idServ.Run()
+	return nil
+}
+
+// 服务停止
+func (p *program) Stop(s service.Service) error {
+	// Any work in Stop should be quick, usually a few seconds at most.
+	goutils.LogInfo(goutils.GetCurrentPath()+"run.log", "GoSnowflake Stopping...")
+	idServ.Stop()
+	goutils.LogInfo(goutils.GetCurrentPath()+"run.log", "GoSnowflake StStopped!")
+	return nil
 }
