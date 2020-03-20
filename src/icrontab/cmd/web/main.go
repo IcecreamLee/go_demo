0b953bdf7584f041e54c8c394e54dc13e522b148
Icecream <icecream-lee@qq.com>
2020-03-20 18:22:20 +0800

    update icrontab

--- src/icrontab/cmd/web/main.go
+++ src/icrontab/cmd/web/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"github.com/IcecreamLee/goutils"
 	"github.com/gin-gonic/gin"
+	"html/template"
 	"icrontab/cmd/web/routers"
 	"icrontab/internal/config"
 )
@@ -9,8 +11,15 @@ import (
 func main() {
 	r := gin.Default()
 	routers.SetRouters(r)
+
+	r.SetFuncMap(template.FuncMap{
+		"formatDate":     goutils.DateFormat,
+		"formatDatetime": goutils.DatetimeFormat,
+	})
+
 	r.Static("/static", "./statics")
 	r.StaticFile("/favicon.ico", "./statics/favicon.ico")
 	r.LoadHTMLGlob("views/*")
+
 	r.Run(":" + config.WebServicePort) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
